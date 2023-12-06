1ff076a52f8d889404a933cacf331a657b99afd6
muhamadazz <153073061+muhamadazz@users.noreply.github.com>
2023-12-06 23:42:46 +0700

    Update index.js

--- index.js
+++ index.js
@@ -17,4 +17,39 @@ fetch(`http://localhost:3000/mahasiswa/${mahasiswaNim}`, {
     .then(data => console.log(data))
     .catch(error => console.error('Error:', error));
 
+
+// POST
+const postData = {
+    nim: '1102030',
+    nama: 'iyom',
+    gender: 'P',
+    prodi: 'TE',
+    alamat: 'Jl. cikaso no.130'
+};
+
+fetch('http://localhost:3000/mahasiswa', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(postData)
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
+
+// DELETE
+fetch('http://localhost:3000/mahasiswa/1102023', {
+    method: 'DELETE',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+})
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+
+
+
     
